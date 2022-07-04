3bc1d5cdb6c06228529e8f06b23833ffc347e639
Simone Brin <simonebrin@gmail.com>
2022-07-04 09:51:44 -0400

    added delete task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
+var pageContentEl = document.querySelector("#page-content");
 var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+
 
 var taskFormHandler = function(event) {
   event.preventDefault();
@@ -86,3 +88,21 @@ return actionContainerEl;
 }
 
 formEl.addEventListener("submit", taskFormHandler);
+
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+  };
+
+var taskButtonHandler = function(event) {
+    console.log(event.target);
+  
+    if (event.target.matches(".delete-btn")) {
+        // get the element's task id
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+        console.log(taskId);
+    }
+  };
+
+pageContentEl.addEventListener("click", taskButtonHandler);
